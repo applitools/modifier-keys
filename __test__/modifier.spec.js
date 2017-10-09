62983d1c60c550552408d7a258535db8974ee8f1
Tomer <tomer@corevo.io>
2017-10-09 12:16:45 +0300

    macOS tests

--- __test__/modifier.spec.js
+++ __test__/modifier.spec.js
@@ -1,9 +1,15 @@
 /* eslint-disable */
 import modifier from '../src/modifier';
 
-describe('modifier closure', () => {
+describe('modifier macOS', () => {
   it('should treat command as primary key', () => {
     let event = new KeyboardEvent('keydown', {'key': 'c', 'metaKey': true});
-    expect(modifier(event).withPrimaryKey).toBeTruthy();
+    expect(modifier(event).primaryKey).toBeTruthy();
+    expect(modifier(event).secondaryKey).toBeFalsey();
+  });
+  it('should treat options as secondary key', () => {
+    let event = new KeyboardEvent('keydown', {'key': 'c', 'altKey': true});
+    expect(modifier(event).primaryKey).toBeFalsey();
+    expect(modifier(event).secondaryKey).toBeTruthy();
   });
 });
