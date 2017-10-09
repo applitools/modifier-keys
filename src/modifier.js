86dca60456d901bf23e25612c9473b0943a823b5
Tomer <tomer@corevo.io>
2017-10-09 15:24:12 +0300

    parse the useragent

--- src/modifier.js
+++ src/modifier.js
@@ -1,3 +1,24 @@
+import parser from 'ua-parser-js';
+import useragent from './useragent';
+
+const OS = {
+  macOS: 0,
+  Windows: 1,
+  other: 2
+};
+
+function getOS(currUseragent) {
+  let ua = parser(currUseragent);
+  switch (ua.os.name) {
+    case 'Mac OS':
+      return OS.macOS;
+    case 'Windows':
+      return OS.Windows;
+    default:
+      return OS.other;
+  }
+}
+
 export default function Modifier(handler) {
   return (event, ...argv) => {
     return handler(modifier(event), ...argv);
@@ -5,5 +26,8 @@ export default function Modifier(handler) {
 }
 
 export function modifier(event) {
+  const os = getOS(useragent);
+  event.primaryKey = ((os === OS.macOS && event.metaKey) || (os !== OS.macOS && event.ctrlKey));
+  event.secondaryKey = event.altKey;
   return event;
 }
