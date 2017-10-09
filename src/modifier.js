61014a373b2e3911f2a9e77b06f06f80f7508dfa
Tomer <tomer@corevo.io>
2017-10-09 14:31:13 +0300

    implemented the closure

--- src/modifier.js
+++ src/modifier.js
@@ -1,5 +1,9 @@
-export default function Modifier() {
+export default function Modifier(handler) {
+  return (event, ...argv) => {
+    return handler(modifier(event), ...argv);
+  };
 }
 
-export function modifier() {
+export function modifier(event) {
+  return event;
 }
