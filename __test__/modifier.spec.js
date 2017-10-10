543a66e4de2ff46c7ee57f4fcfacdaf56d56d815
Tomer <tomer@corevo.io>
2017-10-10 11:30:18 +0300

    added linux tests

--- __test__/modifier.spec.js
+++ __test__/modifier.spec.js
@@ -33,6 +33,21 @@ describe('modifier Windows', () => {
   });
 });
 
+describe('modifier Linux', () => {
+  it('should treat control as primary key', () => {
+    getUserAgentMock.mockReturnValue("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.0 Safari/537.36");
+    let event = new KeyboardEvent('keydown', {'key': 'c', 'ctrlKey': true});
+    expect(modifier(event).primaryKey).toBeTruthy();
+    expect(modifier(event).secondaryKey).toBeFalsy();
+  });
+  it('should treat alt as secondary key', () => {
+    getUserAgentMock.mockReturnValue("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.0 Safari/537.36");
+    let event = new KeyboardEvent('keydown', {'key': 'c', 'altKey': true});
+    expect(modifier(event).primaryKey).toBeFalsy();
+    expect(modifier(event).secondaryKey).toBeTruthy();
+  });
+});
+
 describe('modifier closure', () => {
   it('should add the primary key to the event handler', () => {
     getUserAgentMock.mockReturnValue("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3235.0 Safari/537.36");
